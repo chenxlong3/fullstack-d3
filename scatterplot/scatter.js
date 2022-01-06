373a765839d755153e9f7d41fd4188e8401c2866
chenxlong3 <1226608021@qq.com>
2022-01-06 23:26:00 +0800

    bookname

--- scatterplot/scatter.js
+++ scatterplot/scatter.js
@@ -2,7 +2,9 @@ async function drawScatter() {
     let dataset = await d3.json("../data/weather_data.json");
     const x_accessor = d => d.dewPoint;
     const y_accessor = d => d.humidity;
-
+    // TextDecoderStream
+    // tesata
+    // teatae
 }
 
 drawScatter();
