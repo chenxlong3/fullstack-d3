950615871d2a3060febade41df8867cea5be8a80
chenxlong3 <1226608021@qq.com>
2022-01-07 23:31:11 +0800

    bars prototype

--- barchart/barChart.js
+++ barchart/barChart.js
@@ -18,33 +18,47 @@ async function barChart() {
     dimensions.bounded_height = dimensions.width - dimensions.margin.bottom - dimensions.margin.top;
 
     const wrapper = d3.select("#wrapper")
-    .append("svg")
-    .attr("width", dimensions.bounded_width)
-    .attr("height", dimensions.bounded_height);
+        .append("svg")
+        .attr("width", dimensions.bounded_width)
+        .attr("height", dimensions.bounded_height);
 
     const bounds = wrapper.append("g")
-    .style("transform", `translate(
+        .style("transform", `translate(
         ${dimensions.margin.left}px,
         ${dimensions.margin.top}px
     )`);
 
     const xScale = d3.scaleLinear()
-    .domain(d3.extent(dataset, dAccessor))
-    .range([0, dimensions.bounded_width])
-    .nice();
+        .domain(d3.extent(dataset, dAccessor))
+        .range([0, dimensions.bounded_width])
+        .nice();
 
     const binsGen = d3.histogram()
-    .domain(xScale.domain())
-    .value(dAccessor)
-    .thresholds(12);
+        .domain(xScale.domain())
+        .value(dAccessor)
+        .thresholds(12);
 
     const bins = binsGen(dataset);
     console.log(bins);
 
     const yAccessor = d => d.length;
     const yScale = d3.scaleLinear()
-    .domain([0, d3.max(bins, yAccessor)])
-    .range([dimensions.bounded_height, 0]);
+        .domain([0, d3.max(bins, yAccessor)])
+        .range([dimensions.bounded_height, 0])
+        .nice();
+
+    const binsGroup = bounds.append("g");
+    const binsGroups = binsGroup.selectAll("g")
+        .data(bins)
+        .enter().append("g");
+
+    const bar_padding = 1;
+    const bar_rects = binsGroups.append('rect')
+        .attr('x', d => xScale(d.x0) + bar_padding / 2)
+        .attr('y', d => yScale(yAccessor(d)))
+        .attr('width', d => d3.max([0, xScale(d.x1) - xScale(d.x0) - bar_padding]))
+        .attr('height', d => dimensions.bounded_height - yScale(yAccessor(d)))
+        .style('fill', 'cornflowerblue');
 
 }
 
